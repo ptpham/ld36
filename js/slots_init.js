937da2140883bc4536d58aac148a282f52a25392
Peter Pham <ptr.pham@gmail.com>
2016-08-27 12:22:41 -0700

    Adding encounter available slots to layout.

--- js/slots_init.js
+++ js/slots_init.js
@@ -8,6 +8,7 @@ function makeSlots(target, count) {
 }
 
 makeSlots(elements.inventory.slots, constants.inventory.size);
+makeSlots(elements.available.slots, constants.available.size);
 makeSlots(elements.request.slots, constants.request.size);
 makeSlots(elements.offer.slots, constants.offer.size);
 
