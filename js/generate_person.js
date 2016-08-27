12b06b6302da09936835f78dbbf519087193056c
Cindy Zhang <cixzhang@gmail.com>
2016-08-27 11:39:21 -0700

    adding wants

--- js/generate_person.js
+++ js/generate_person.js
@@ -1,9 +1,30 @@
 
+(function () {
+
+var wants = constructWants(constants.items);
+
 generatePerson = objgen.compile([{
   name: { $options: { 'bob': 10, 'alice': 20 } },
   age: { $integer: [5, 60] },
+  desire: { $integer: [-1, 2]},
+  wants: wants
 }, {
-  $conditions: { age: { $gt: 18 } },
-  greeting: { $options: { "I'm an adult!": 1 } }
+  $conditions: { desire: {$lt: 0} },
+  greeting: { $integer: [0, 3]} // curmudgeons
+}, {
+  $conditions: { desire: {$eq: 0} },
+  greeting: { $integer: [3, 6]} // normal
+}, {
+  $conditions: { desire: {$gt: 0} },
+  greeting: { $integer: [6, 9]} // fanatic
 }]);
 
+function constructWants(items) {
+  var wants = {};
+  items.forEach(function (item) {
+    wants[item.name] = { $integer: [-3, 4] };
+  });
+  return wants;
+}
+
+})()
