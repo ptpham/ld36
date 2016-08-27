8269438ad876ce2baf418e44860fcba722df7801
Peter Pham <ptr.pham@gmail.com>
2016-08-27 15:44:50 -0700

    Adding logic to check for victory condition.

--- js/logic_ship.js
+++ js/logic_ship.js
@@ -1,6 +1,14 @@
 
 (function() {
 
+function detectVictory() {
+  var slots = elements.ships.slots.children;
+  for (var i = 0; i < slots.length; i++) {
+    if (slots[i].dataset.id == null) return false;
+  }
+  return true;
+}
+
 document.addEventListener('click', function(e) {
   var ship = e.target.closest('ship');
   if (ship == null) return;
@@ -21,11 +29,16 @@ document.addEventListener('item-slot:swap', function(e) {
 
   var target = meta.items.all[dst.dataset.id];
   var candidate = meta.items.all[src.dataset.id];
+  if (candidate == null || target.goal == null) return;
 
   if (target.goal == candidate.name) {
     dst.dataset.id = src.dataset.id;
   }
   delete src.dataset.id;
+
+  if (detectVictory()) {
+    this.dispatchEvent(new CustomEvent('game:victory', { bubbles: true }));
+  }
 });
 
 })();
