69cbb5aec197a3463b17117f5630f4fb868ff88c
Peter Pham <ptr.pham@gmail.com>
2016-08-27 15:07:43 -0700

    Adding encounter generation and cycling.

--- js/logic_encounter.js
+++ js/logic_encounter.js
@@ -1,5 +1,21 @@
 
 utilities.encounter = (function() {
+
+  function generate() {
+    var personProbaility =  0.5;
+    if (Math.random() < personProbaility) {
+      return { person: utilities.person.generate() };
+    }
+    return { environment: utilities.environment.generate() };
+  }
+
+  function applyEncounter(encounter) {
+    if (encounter.person != null) {
+      applyPerson(encounter.person);
+    } else if (encounter.environment != null) {
+      applyEnvironment(encounter.environment);
+    }
+  }
   
   function applyEnvironment(environment, encounter) {
     encounter = encounter || document.querySelector('encounter');
@@ -11,9 +27,21 @@ utilities.encounter = (function() {
     encounter.setAttribute('class', 'person'); 
   }
 
+  function newEncounter() { applyEncounter(generate()); }
+
   return {
+    applyEncounter,
     applyEnvironment,
-    applyPerson
+    applyPerson,
+    newEncounter,
+    generate,
   };
 })();
+
+document.addEventListener('click', function(e) {
+  if (!e.target.matches('button[data-intent="leave"]')) return;
+  utilities.encounter.newEncounter();
+});
+
+utilities.encounter.newEncounter();
 
