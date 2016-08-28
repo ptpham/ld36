fb9a112a73c5bdcd9be0a39e740975146317c2d7
Peter Pham <ptr.pham@gmail.com>
2016-08-27 19:55:49 -0700

    Removing selection highlighting and fixing item-slot emptiness when swapping.

--- js/logic_trade.js
+++ js/logic_trade.js
@@ -27,10 +27,13 @@ document.addEventListener('item-slot:swap', function(e) {
 
   if (srcParent != dstParent &&
     !swappable.get(srcParent).has(dstParent)) return;
-
   var swap = src.dataset.id;
+
   src.dataset.id = dst.dataset.id;
+  if (dst.dataset.id == null) delete src.dataset.id;
+
   dst.dataset.id = swap;
+  if (swap == null) delete dst.dataset.id;
 });
 
 })();
