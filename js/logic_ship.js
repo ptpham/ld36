75ab784686c770a2299473dd82bdc7ab2eca5c3d
Peter Pham <ptr.pham@gmail.com>
2016-08-28 12:18:20 -0700

    Tweaking style.

--- js/logic_ship.js
+++ js/logic_ship.js
@@ -13,7 +13,7 @@ document.addEventListener('click', function(e) {
   var ship = e.target.closest('ship');
   if (ship == null) return;
 
-  if (ship.querySelector('expand').contains(e.target)) {
+  if (ship.querySelector('.expander').contains(e.target)) {
     ship.classList.add('expanded');
   } else if (ship.querySelector('collapse').contains(e.target)) {
     ship.classList.remove('expanded');
